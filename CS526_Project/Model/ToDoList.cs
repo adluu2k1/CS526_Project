a10e8ffbb8ce355fc590df0b7dbffadff00a9f61
anhdungbmt2001 <anhdungbmt2001@gmail.com>
2023-01-17 15:14:59 +0700

    Update ToDo_Task class & Create sample ToDo_List

--- CS526_Project/Model/ToDoList.cs
+++ CS526_Project/Model/ToDoList.cs
@@ -1,22 +1,26 @@
-﻿using System;
+﻿using Android.OS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks
+using System.Threading.Tasks;
 
 namespace CS526_Project.Model
 {
-    public class ToDoList
+    public class ToDo_Task
     {
         public int id { get; set; }
         public string Description { get; set; } = string.Empty;
-        public int CategoryId { get; set; }
-        public DateTime StartDate { get; set; } = DateTime.now;
-        public DateTime EndDate { get; set; } = DateTime.Today.AddDays(1); // Deadline 1 day by default
-        public bool Done { get; set; } = false;
+        public List<int> CategoryId { get; set; }
+        public DateTime AddTime { get; set; } = DateTime.Now;
+        public List<DateTime> NotificationTime { get; set; }
+        public DateTime DeadlineTime { get; set; } = DateTime.Today.AddDays(1); // Deadline 1 day by default
+        public DateTime EndTime { get; set; }
+        public bool IsDone { get; set; } = false;
+
         public void AddTask()
         {
-
+            
         }
         public void EditTask()
         {
