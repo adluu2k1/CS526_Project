63062a86ed3bd4c68634533dda9144e8cd30ef68
anhdungbmt2001 <anhdungbmt2001@gmail.com>
2023-01-24 20:20:02 +0700

    Fix ToDoList initializing null property

--- CS526_Project/Model/ToDoList.cs
+++ CS526_Project/Model/ToDoList.cs
@@ -1,5 +1,4 @@
-﻿using Android.OS;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +10,9 @@ namespace CS526_Project.Model
     {
         public int id { get; set; }
         public string Description { get; set; } = string.Empty;
-        public List<int> CategoryId { get; set; }
+        public List<int> CategoryId { get; set; } = new List<int>();
         public DateTime AddTime { get; set; } = DateTime.Now;
-        public List<DateTime> NotificationTime { get; set; }
+        public List<DateTime> NotificationTime { get; set; } = new List<DateTime>();
         public DateTime DeadlineTime { get; set; } = DateTime.Today.AddDays(1); // Deadline 1 day by default
         public DateTime EndTime { get; set; }
         public bool IsDone { get; set; } = false;
