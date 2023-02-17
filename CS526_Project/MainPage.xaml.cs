1d787b5400d5cd408350314dcf8af1b78c1f2c2e
Thanh Thao <thanhthaonguyen1082001@gmail.com>
2023-02-17 12:52:41 +0700

    ádasd

--- CS526_Project/MainPage.xaml.cs
+++ CS526_Project/MainPage.xaml.cs
@@ -18,12 +18,10 @@ public partial class MainPage : ContentPage
 
 	public void ShowTask(DateTime date)
 	{
-		var ListAllTask = App.Database.GetAllTask();
-
-		// TODO: filter by Due Date (Deadlinetime)
+		var ListAllTask = App.Database.GetAllTask().OrderBy(p => p.DeadlineTime).ToList();
 		foreach (var task in ListAllTask) 
 		{
-			if (DateTime.Compare(DateTime.Now, task.DeadlineTime) <=0);
+			if (DateTime.Compare(DateTime.Now, task.DeadlineTime) <= 0)
 			{
                 TaskViewWrapper.Add(new TaskView(task, TaskViewWrapper));
             }
